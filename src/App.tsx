5c5121333da0ae48b8080cdb637ecbad658985d9
alexaniatoma <alexaniatoma@hotmail.com>
2025-04-08 16:54:36 -0300

    adicionado service e atualizado cadastrotsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} /> 
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
             </Routes>
           </div>
