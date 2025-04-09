0488d998899ecb4978a9acb4ea85cfd07eed3885
alexaniatoma <alexaniatoma@hotmail.com>
2025-04-09 12:27:34 -0300

    componentes navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 function Navbar() {
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+        
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+
     return (
         <>
             <div className='w-full flex justify-center py-4
@@ -13,8 +26,9 @@ function Navbar() {
                         Postagens
                         Temas
                         Cadastrar tema
-                        Perfil
-                        Sair
+                        Perfil 
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                        
                     </div>
                 </div>
             </div>
